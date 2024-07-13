431d8b02f4e5c8273576ff9f41b3a149653f73e9
Nikhil2510-java <nikhilborutkar@gmail.com>
2024-07-13 20:42:42 +0530

    DEV-Edited payment madule

--- Git-Maven-Proj01/src/main/java/com/nt/sbean/Payment.java
+++ Git-Maven-Proj01/src/main/java/com/nt/sbean/Payment.java
@@ -2,8 +2,13 @@ package com.nt.sbean;
 
 public class Payment {
 	
-	public void OnlinePayment() {
+	public void onlinePayment() {
 		System.out.println("Online Payment LOGIC");
+		System.out.println("Online Payment Updated Suceefully");
+	}
+	
+	public void paymentBHIM() {
+		System.out.println("Pay Using BHIM Paymnet MOde ");
 	}
 
 }
